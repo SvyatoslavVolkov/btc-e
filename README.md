e0e21b527147159b4005ecefa21aca80e6245609
acidvegas <acidvegas@users.noreply.github.com>
2017-05-11 21:46:47 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# btc-e
+A Python class for the Public/Trade API on BTC-e.com
+
+###### API Documentation
+ - [Public API v3](https://btc-e.com/api/3/docs)
+ - [Trade  API v1](https://btc-e.com/tapi/docs)
---
+++ btce/example.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# BTC-e API Class (Example Script)
+# Developed by acidvegas in Python 3
+# https://github.com/acidvegas/btce
+# example.py
+
+'''
+Here is an example script that uses both the Public API & Trade API.
+The script will retrieve your balance for each coin and the total sum.
+Coins with no balance are not showed.
+'''
+
+from btce import public_api, trade_api
+
+# API Keys
+api_key    = 'CHANGEME'
+api_secret = 'CHANGEME'
+nonce      = 1
+
+# Main
+tapi = trade_api(api_key, api_secret, nonce)
+info = tapi.getInfo()
+if info['success']:
+	funds = info['return']['funds']
+	total = 0.00
+	print('COIN   BALANCE     PRICE        VALUE')
+	for coin in funds:
+		if funds[coin]:
+			balance = '{0:.2f}'.format(funds[coin])
+			price   = '{0:.2f}'.format(public_api.ticker(coin, 'usd')[f'{coin}_usd']['sell'])
+			value   = '{0:.2f}'.format(float(balance) * float(price))
+			total += float(value)
+			print('{0}{1}${2}${3}'.format(coin.ljust(7, ' '), balance.ljust(12, ' '), price.ljust(12, ' '), value))
+	print('Total: ${0:.2f}'.format(total))
+else:
+	print('[!] Error - ' + info['error'])
